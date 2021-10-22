e2f59e4635e1e20ff1745d61927c8163efb1edd2
DAI ANDO <daifloss@gmail.com>
2021-10-22 16:11:18 +0900

    20211022 #2

--- algo/sum/twonumbersum2.py
+++ algo/sum/twonumbersum2.py
@@ -10,12 +10,5 @@ def twoNumberSum(array, targetSum):
 
 
 
-        a1 = array
-        a2 = array
-    	ret = list()
 
-        for x in a1:
-            if a1.count(targetSum - x) != 0 and x != targetSum - x:
-                ret += [ x ]
-
-    	return ret
+# O(n) time | O(n) space
